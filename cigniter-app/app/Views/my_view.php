8a7a2413ed029693912745898a1780a8843bf88e
clopez-stickerapp <cristian@stickerapp.com>
2024-01-25 16:27:57 +0100

    tested DB, start moving old caves to new site

--- cigniter-app/app/Views/my_view.php
+++ cigniter-app/app/Views/my_view.php
@@ -1,11 +1,3 @@
 <?php
-$db = \Config\Database::connect();
-
-$query   = $db->query('SELECT * FROM thecave_materials');
-$results = $query->getResult();
-
-foreach ($results as $row) {
-    var_dump($row);
-}
 echo "This is my first view!!!";
 ?>
