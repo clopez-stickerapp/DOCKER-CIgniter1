4946842f3d693d1ee5f0af69606d79804b39f7d1
clopez-stickerapp <cristian@stickerapp.com>
2024-01-29 14:49:15 +0100

    404 file not found

--- cigniter-app/app/Controllers/Files.php
+++ cigniter-app/app/Controllers/Files.php
@@ -5,10 +5,14 @@ namespace App\Controllers;
 class Files extends BaseController
 {
     public function view( $file ) {
-        $PATH = getcwd();
-        $myimage = file_get_contents( $PATH . '/public/uploads/' . $file);
-        header('Content-Type: image/jpg');
-        echo $myimage;
+        try {
+            $PATH = getcwd();
+            $myimage = file_get_contents( $PATH . '/public/uploads/' . $file);
+            header('Content-Type: image/jpg');
+            echo $myimage;
+        } catch ( \Exception $e ) {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+        }
         exit;
     }
 }
